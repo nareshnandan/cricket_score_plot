f268d298a8f990f92d892697cd51cf492170d69d
nareshnandan <nareshnandan23@gmail.com>
2025-06-03 06:54:48 +0530

    Updated plot: Added wicket markers and removed old markers

--- cricket_analysis.py
+++ cricket_analysis.py
@@ -4,9 +4,21 @@ import matplotlib.pyplot as plt
 
 score = pd.read_csv('cricket_score.csv')
 
+# wickets fell at these overs
+india_wickets = [3, 7, 12, 17] 
+australia_wickets = [2, 4, 8, 10, 11, 14, 15, 16, 19]
+
 plt.figure(figsize=(10,5))
-plt.plot(score["Over"], score["India"], marker='o', linestyle='-', color='blue', label="India")
-plt.plot(score["Over"], score["Australia"], marker='s', linestyle='-', color='orange', label="Australia")
+plt.plot(score["Over"], score["India"], linestyle='-', color='blue', label="India")
+plt.plot(score["Over"], score["Australia"], linestyle='-', color='orange', label="Australia")
+
+# Add wickets as big red and green circles using scatter
+plt.scatter(india_wickets, score.loc[score["Over"].isin(india_wickets), "India"],
+            color='red', s=100, label='India Wickets')
+
+plt.scatter(australia_wickets, score.loc[score["Over"].isin(australia_wickets), "Australia"],
+            color='green', s=100, label='Australia Wickets')
+
 plt.xlabel("Over")
 plt.ylabel("Runs")
 plt.title("Cricket Score Comparison (India vs Australia)")
